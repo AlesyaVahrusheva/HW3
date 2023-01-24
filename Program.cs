480c3637c2c518647e5c26b0f1c053a8bfbd01ed
AlesyaVahrusheva <vahrushevaao@yandex.ru>
2023-01-24 15:54:01 +0300

    3HW

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//принимает на ввод число и выдает является ли оно четным
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("yes");
+}
+else
+{
+    Console.Write("No");
+}
+
